30b9f590b50c4a079025173245a7a6a45825a784
PriyanshuNamdev <namdevpriyanshu44@gmail.com>
2024-04-23 23:49:26 +0530

    Polymorphism

---
+++ OOPs/9_Polymorphism.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self):
+        pass
+    def Exp(self):
+        print("None")
+
+class Player1(Player):
+    def __init__(self,name,character):
+        self.name=name
+        self.character=character
+
+    def Exp(self,exp_lvl=0):
+        self.exp_lvl=exp_lvl
+        print(f"Player {self.name} has a {self.character} of Exp level: {self.exp_lvl}")
+    
+p1=Player1("Priyanshu","Wizard")
+p1.Exp(50)
+
+        
